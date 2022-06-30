564602b95d5030ec19ecc2f0545bdb81030eebea
Elias Eriksson <eliaseriksson95@gmail.com>
2022-06-30 16:18:32 +0200

    removed nulls, renamed websocket property.

--- Response/Like.ts
+++ Response/Like.ts
@@ -5,16 +5,20 @@ export interface Like {
 	header?: Header
 	body?: any | Promise<any>
 	webSocket?: WebSocket
+	socket?: WebSocket
 }
 
 export namespace Like {
 	export function is(value: Like | any): value is Like {
 		return (
 			typeof value == "object" &&
-			Object.keys(value).every(key => ["status", "header", "body", "webSocket"].some(property => property == key)) &&
+			Object.keys(value).every(key =>
+				["status", "header", "body", "webSocket", "socket"].some(property => property == key)
+			) &&
 			(value.status == undefined || typeof value.status == "number") &&
 			(value.header == undefined || Header.is(value.header)) &&
 			(value.webSocket == undefined || value.webSocket instanceof WebSocket) &&
+			(value.webSocket == undefined || value.socket instanceof WebSocket) &&
 			(value.status != undefined || value.header != undefined || value.body != undefined)
 		)
 	}
--- Response/index.ts
+++ Response/index.ts
@@ -7,41 +7,42 @@ export interface HttpResponse {
 	readonly status: number
 	readonly header: Readonly<ResponseHeader>
 	readonly body?: any | Promise<any>
-	readonly webSocket?: WebSocket | null
+	readonly socket?: WebSocket
 }
 
 export namespace HttpResponse {
 	export function is(value: any | HttpResponse): value is HttpResponse {
 		return (
 			typeof value == "object" &&
-			Object.keys(value).every(key => ["status", "header", "body", "webSocket"].some(k => k == key)) &&
+			Object.keys(value).every(key => ["status", "header", "body", "socket"].some(k => k == key)) &&
 			(value.status == undefined || typeof value.status == "number") &&
 			(value.header == undefined || ResponseHeader.is(value.header)) &&
-			(value.webSocket == undefined || value.webSocket instanceof WebSocket)
+			(value.socket == undefined || value.socket instanceof WebSocket)
 		)
 	}
 	export async function to(request: HttpResponse): Promise<Response> {
 		return new Response(await Serializer.serialize(await request.body, request.header.contentType), {
 			status: request.status,
 			headers: new Headers(ResponseHeader.to(request.header) as Record<string, string>),
-			webSocket: request.webSocket,
-		} as ResponseInit & Partial<{ webSocket: WebSocket | null }>)
+			...(request.socket && { webSocket: request.socket }),
+		} as ResponseInit & { webSocket?: WebSocket | null })
 	}
-	export function from(response: Response & Partial<{ webSocket?: WebSocket | null }>): HttpResponse {
+	export function from(response: Response & { webSocket?: WebSocket | null }): HttpResponse {
 		return {
 			status: response.status,
 			header: ResponseHeader.from(response.headers),
-			body: response.status == 101 ? null : Parser.parse(response),
-			webSocket: response.webSocket ?? undefined,
+			body: response.status == 101 ? undefined : Parser.parse(response),
+			...(response.webSocket && { socket: response.webSocket }),
 		}
 	}
 	export function create(response: ResponseLike | any, contentType?: string): HttpResponse {
-		const result: Required<Omit<ResponseLike, "webSocket">> = ResponseLike.is(response)
+		const result: Required<Omit<ResponseLike, "webSocket" | "socket">> = ResponseLike.is(response)
 			? {
 					status: 200,
 					header: {},
 					body: undefined,
-					...(response.webSocket && { webSocket: response.webSocket }),
+					...((response.webSocket && { socket: response.webSocket }) ||
+						(response.socket && { socket: response.socket })),
 					...response,
 			  }
 			: {
@@ -59,7 +60,8 @@ export namespace HttpResponse {
 						typeof response == "object" && !Array.isArray(response)
 							? (({ header, ...body }) => body)(response)
 							: response,
-					...(response.webSocket && { webSocket: response.webSocket }),
+					...((response.webSocket && { socket: response.webSocket }) ||
+						(response.socket && { socket: response.socket })),
 			  }
 		if (!result.header.contentType)
 			switch (typeof result.body) {
