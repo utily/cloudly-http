818706e320cd3ac156cfbdd980dda1830cc24ef1
Simon Mika <simonmika@users.noreply.github.com>
2022-06-30 12:09:43 +0200

    Fixed issue with undefined keys in parameter and search. (#42)
    * Fixed issue with undefined keys in parameter and search.
    
    * Fixed linting.

--- Request/Like.ts
+++ Request/Like.ts
@@ -4,8 +4,8 @@ import { Header } from "./Header"
 export interface Like {
 	method?: string | Method
 	url: string | URL
-	parameter?: { [key: string]: string }
-	readonly search?: { readonly [key: string]: string }
+	parameter?: { [key: string]: string | undefined }
+	readonly search?: { readonly [key: string]: string | undefined }
 	readonly remote?: string
 	readonly header?: Header
 	readonly body?: any | Promise<any>
