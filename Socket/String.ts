85c987245696914af00b743e94ac2f03a67d4429
Elias Eriksson <eliaseriksson95@gmail.com>
2022-07-06 15:47:31 +0200

    exposed onclose and onopen for string socket

--- Socket/String.ts
+++ Socket/String.ts
@@ -7,6 +7,12 @@ export class String {
 	set listen(value: ((string: string) => void) | undefined) {
 		this.backend.listen = value && (string => value(new TextDecoder().decode(string)))
 	}
+	set onclose(value: () => void) {
+		this.backend.onclose = value
+	}
+	set onopen(value: () => void) {
+		this.backend.onopen = value
+	}
 	constructor(private readonly backend: ArrayBuffer) {}
 	send(string: string): void {
 		this.backend.send(new TextEncoder().encode(string))
