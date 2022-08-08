f7007d7d0d59e5f1d964bfd77b2e54f052a2de3f
Elias Eriksson <eliaseriksson95@gmail.com>
2022-08-08 12:04:38 +0200

    properties above constructor

--- Socket/Socket.ts
+++ Socket/Socket.ts
@@ -6,32 +6,6 @@ export abstract class Socket<T = any> {
 	#sendQueue?: Message[] = []
 	#closed = false
 	#backend: Backend
-	constructor(backend: Backend, isOpen?: boolean) {
-		this.#backend = backend
-		if (isOpen)
-			this.#sendQueue = undefined
-		this.#backend.addEventListener("open", () => {
-			if (this.#onOpen)
-				this.#onOpen()
-			this.#handleOpen.bind(this)
-		})
-		this.#backend.addEventListener("message", async (event: MessageEvent) => {
-			const data = this.processReceived(event.data)
-			this.#handleOpen()
-			if (this.#onMessage)
-				this.#onMessage(data)
-			else
-				this.#receiveQueue?.push(data)
-		})
-		this.#backend.addEventListener("close", () => {
-			if (this.#onClose)
-				this.#onClose()
-			this.#closed = true
-		})
-	}
-	protected abstract processReceived(data: Message): T
-	protected abstract processSend(data: T): Message
-
 	get state(): "opened" | "closed" {
 		return this.#closed ? "closed" : "opened"
 	}
@@ -59,6 +33,31 @@ export abstract class Socket<T = any> {
 			value()
 		this.#onOpen = value
 	}
+	constructor(backend: Backend, isOpen?: boolean) {
+		this.#backend = backend
+		if (isOpen)
+			this.#sendQueue = undefined
+		this.#backend.addEventListener("open", () => {
+			if (this.#onOpen)
+				this.#onOpen()
+			this.#handleOpen.bind(this)
+		})
+		this.#backend.addEventListener("message", async (event: MessageEvent) => {
+			const data = this.processReceived(event.data)
+			this.#handleOpen()
+			if (this.#onMessage)
+				this.#onMessage(data)
+			else
+				this.#receiveQueue?.push(data)
+		})
+		this.#backend.addEventListener("close", () => {
+			if (this.#onClose)
+				this.#onClose()
+			this.#closed = true
+		})
+	}
+	protected abstract processReceived(data: Message): T
+	protected abstract processSend(data: T): Message
 	send(message: T): void {
 		if (this.#sendQueue)
 			this.#sendQueue.push(this.processSend(message))
