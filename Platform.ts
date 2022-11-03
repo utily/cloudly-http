77fe65e50e0baffe5bf467e9712be63d2a0bd6fe
Robin Nedergård <60611967+robinnedergard@users.noreply.github.com>
2022-11-03 10:26:24 +0100

    Added fallback for Response ?? globalThis.Response (#44)
    * Added fallback for Response ?? globalThis.Response
    
    * Removed vulnerability + bumped prettierx

--- Platform.ts
+++ Platform.ts
@@ -1,7 +1,7 @@
-const PlatformRequest = Request
+const PlatformRequest = typeof Request != "undefined" ? Request : globalThis.Request
 type PlatformRequest = Request
 
-const PlatformResponse = Response
+const PlatformResponse = typeof Response != "undefined" ? Response : globalThis.Response
 type PlatformResponse = Response
 
 export { PlatformRequest as Request, PlatformResponse as Response }
