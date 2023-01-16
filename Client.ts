83b1bf02bc94be1981d8757cfe8f875ee3707aff
Lasse Löfquist <tvartom@users.noreply.github.com>
2023-01-16 09:59:28 +0100

    Improved callbacks for Client (#46)
    * Improved callbacks for Client
    
    * Removed default / in Client.fetch
    
    * Documentation for breaking changes

--- Client.ts
+++ Client.ts
@@ -4,32 +4,40 @@ import { Request } from "./Request"
 import { Response } from "./Response"
 
 export class Client<Error = never> {
+	preprocess = async (request: Request): Promise<Request> => request
+	getHeader? = async (request: Request) => ({
+		...(request.body ? { contentType: "application/json; charset=utf-8" } : undefined),
+		...(this.key ? { authorization: `Bearer ${this.key}` } : undefined),
+		...this.appendHeader?.(request),
+		...request.header,
+	})
+	appendHeader?: (request: Request) => Request.Header
+	postprocess = async (response: Response): Promise<Response> => response
 	onError?: (request: Request, response: Response) => Promise<boolean>
 	onUnauthorized?: (connection: Client<Error>) => Promise<boolean>
-	constructor(public url?: string, public key?: string) {}
 
+	constructor(
+		public url?: string,
+		public key?: string,
+		callbacks?: Partial<
+			Pick<Client<Error>, "preprocess" | "getHeader" | "appendHeader" | "postprocess" | "onError" | "onUnauthorized">
+		>
+	) {
+		Object.assign(this, callbacks)
+	}
+	/**
+	 * @param path Note: Prior to version 0.1.0 this method inserted a `/` between url and path.
+	 */
 	private async fetch<R>(path: string, method: Method, body?: any, header?: Request.Header): Promise<R | Error> {
-		const request = await this.preProcess(Request.create({ url: `${this.url ?? ""}/${path}`, method, header, body }))
-		const response = await this.postProcess(
+		let request = Request.create({ url: `${this.url ?? ""}${path}`, method, header, body })
+		request = await this.preprocess({ ...request, header: (await this.getHeader?.(request)) ?? request.header })
+		const response = await this.postprocess(
 			await fetch(request).catch(error => Response.create({ status: 601, body: error }))
 		)
 		return (response.status == 401 && this.onUnauthorized && (await this.onUnauthorized(this))) ||
 			(response.status >= 300 && this.onError && (await this.onError(request, response)))
 			? await this.fetch<R>(path, method, body, header)
 			: ((await response.body) as R | Error)
-	}
-	protected async preProcess(request: Request): Promise<Request> {
-		return {
-			...request,
-			header: {
-				contentType: request.body ? "application/json; charset=utf-8" : undefined,
-				authorization: this.key ? `Bearer ${this.key}` : undefined,
-				...request.header,
-			},
-		}
-	}
-	protected async postProcess(response: Response): Promise<Response> {
-		return response
 	}
 	async get<R>(path: string, header?: Request.Header): Promise<R | Error> {
 		return await this.fetch<R>(path, "GET", undefined, header)
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Cloud HTTP
+# Cloudly HTTP
 
 Improved handling of HTTP Requests and Responses.
+
+## Breaking changes
+
+### 0.1.0
+
+- Client constructor takes named callbacks.
+- `path`-parameter now should be prefixed with `/`.
