f2c60bb57d8df38f6075620d275d1973fa7e9e98
ChriJ <christofferj93@gmail.com>
2023-01-20 17:01:47 +0100

    initial test

--- fetch.ts
+++ fetch.ts
@@ -13,6 +13,19 @@ fetch.create = create
 function create(fetch: GlobalFetch): Fetch {
 	return async (request: Request.Like | string): Promise<Response> => {
 		const r = Request.create(request)
-		return Response.from(await fetch(r.url.toString(), await Request.to(r)))
+		console.log("r", r)
+		const urlToString = r.url.toString()
+		console.log("urlToString", urlToString)
+		const requestTo = await Request.to(r)
+		console.log("requestTo", requestTo)
+
+		const fetchResponse = await fetch(r.url.toString(), await Request.to(r))
+
+		console.log("fetchResponse", JSON.stringify(fetchResponse))
+		
+		const responseFrom = Response.from(await fetch(r.url.toString(), await Request.to(r)))
+
+		console.log(responseFrom)
+		return responseFrom
 	}
 }
