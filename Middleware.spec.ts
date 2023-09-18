a3d06a811ccaa417de02afd0c6df54a1cbb076d1
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-09-18 11:33:23 +0200

    Fixed cursor. (#56)

--- Middleware.spec.ts
+++ Middleware.spec.ts
@@ -1,5 +1,11 @@
+import { Blob } from "fetch-blob"
+import { File } from "fetch-blob/file"
+import { FormData as Form } from "formdata-polyfill/esm.min.js"
 import { http } from "./index"
 
+globalThis.Blob = Blob
+globalThis.File = File
+globalThis.FormData = Form
 describe("http.Middleware", () => {
 	it("server object", async () => {
 		expect(
