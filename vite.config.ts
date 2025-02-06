fd924505be8fc12b1aaba1c0db7c418ba95462d6
Anton Kroner <anton.kroner@pax2pay.com>
2025-02-06 15:22:58 +0100

    Switched to v8 coverage.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import { defineConfig } from "vite"
 export default defineConfig({
 	test: {
 		typecheck: { tsconfig: "./tsconfig.json" },
-		coverage: { reporter: ["text", "json", "html"], provider: "istanbul" },
+		coverage: {
+			reporter: ["text", "json", "html"],
+			enabled: true,
+			all: true,
+			cleanOnRerun: true,
+			thresholds: { statements: 0, branches: 0, functions: 0, lines: 0 },
+		},
 		globals: true,
 		include: ["**/*.spec.[tj]s"],
 		testTimeout: 20000,
